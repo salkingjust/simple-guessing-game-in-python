628d58bf97238c92a8f86d4bc3a5f62ef5f3a817
David Enabs <68573683+davidenabs@users.noreply.github.com>
2023-09-12 13:49:52 +0100

    First commit

---
+++ guessing-game.py
@@ -0,0 +1,14 @@
+import random
+
+randomNumbers = random.randint(1,20)
+userChoice = 0
+
+while userChoice != randomNumbers:
+    userChoice = int(input("Guess the number in my mind: "))
+    if userChoice < randomNumbers:
+        print("Your choice is too low!")
+
+    elif userChoice > randomNumbers:
+        print("Your choice is too high!")
+
+print("You've quessed correcty!")
