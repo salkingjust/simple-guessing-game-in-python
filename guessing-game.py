f5fd24ccc611378516e38a4e9f97ce8cb5118592
salkingjust <144795808+salkingjust@users.noreply.github.com>
2023-09-12 19:45:28 +0100

    Update guessing-game.py
    is this good enough

--- guessing-game.py
+++ guessing-game.py
@@ -2,13 +2,16 @@ import random
 
 randomNumbers = random.randint(1,100)
 userChoice = 0
+chances = 5
+print("Welcome to the Number Guessing Game!")
+print(f"You have {chances} chances to guess the number between 1 and 100.")
 
-while userChoice != randomNumbers:
-    userChoice = int(input("Guess the number in my mind: "))
+while userChoice != randomNumbers and chances > 0:
+    userChoice = int(input(f"Guess the number in my mind({chances} chances left): "))
     if userChoice < randomNumbers:
         print("Your choice is too low!")
 
     elif userChoice > randomNumbers:
         print("Your choice is too high!")
-
+    chances -= 1
 print("You've quessed correcty!")
